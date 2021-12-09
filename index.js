f9f4b46caf5af47b2249de593adf2f14377b2215
Andy Chen <chenyangpoppin@hotmail.com>
2021-12-09 08:47:45 +0800

    feat: add retrieveMaxDomDepth method

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  let url = 'http://www.tjcchen.cn';
+  let url = 'https://expert.baidu.com';
 
-  let browser = await puppeteer.launch({ headless: false });
+  let browser = await puppeteer.launch({ headless: true });
   let page = await browser.newPage();
 
   await page.goto(url, { waitUntil: 'networkidle2' });
 
-  let data = await page.evaluate(() => {
-    let title = document.querySelector('div[class="flex__item"] > div').innerText;
-    return {
-      title
-    };
-  });
+  const retrieveMaxDomDepth = (root = document) => {
+    let depth = 1, sel = 'body > *';
+    while(root.querySelector(sel)) {
+      sel += ' > *';
+      depth++;
+    }
+    return `The max dom depth is ${depth}.`;
+  };
 
-  let html = await page.$eval('html', el => el.outerHTML);
+  // let data = await page.evaluate(() => {
+  //   let title = document.querySelector('div[class="flex__item"] > div').innerText;
+  //   return {
+  //     title
+  //   };
+  // });
+  // let html = await page.$eval('html', el => el.outerHTML);
+
+  let data = await page.evaluate(retrieveMaxDomDepth);
 
   console.log(data);
 
