79a890a835dcf4527c8be6eeca45114ad0cc7c89
Andy Chen <chenyangpoppin@hotmail.com>
2021-12-29 22:53:06 +0800

    feat: add crawler code logic

--- index2.js
+++ index2.js
@@ -1,43 +1,56 @@
 const puppeteer = require('puppeteer');
 const {log}     = require('console');
 
+// IIFE
 (async () => {
-  let url = 'http://www.tjcchen.cn';
+  let url     = 'http://www.tjcchen.cn';
+  let browser = await puppeteer.launch({headless: true});
+  let page    = await browser.newPage();
 
-  let browser = await puppeteer.launch({ headless: true });
-  let page = await browser.newPage();
-
-  await page.goto(url, { waitUntil: 'networkidle2' });
-
+  await page.goto(url, {waitUntil: 'networkidle2'});
   let data = await page.evaluate(() => {
-    const buildVTree = (root, tree = {}) => {
-      let node;
-      if (!root) {
-          root = document;
-          node = root.querySelector('html');
-      }
-      const childNodes = Array.from(node.childNodes);
-      childNodes.forEach(childNode => {
-          console.log('loop', childNode);
-          if (childNode.nodeType === 1) { // element node
+      let tree = null;
+      let result = [];
+      let count = 0;
+
+      const buildVTree = (node) => {
+          if (!node) {
+              node = document.querySelector('html');
               tree = {
-                  tag: node.tagName,
-                  children: {
-                      tag: childNode.tagName
-                  }
+                  type: node.tagName,
+                  children: []
               };
           }
-          if (childNode.childNodes && childNode.childNodes.length > 0) {
-              buildVTree(childNode, tree);
-          }
-      });
-      return tree;
-    };
+          const childNodes = Array.from(node.childNodes);
 
-    return buildVTree();
+          childNodes.forEach(childNode => {
+              if (childNode.nodeType === 1) { // element node
+                  let subTree = {
+                      type: childNode.tagName
+                  };
+                  // todo 构建subTree children
+                  if (typeof tree.children === 'object') {
+                      tree.children.push(subTree);
+                  } else {
+                      tree.children.children = [];
+                  }
+                  result.push(subTree);
+              }
+              if (childNode.childNodes && childNode.childNodes.length > 0) {
+                  buildVTree(childNode);
+              }
+          });
+
+          return {
+              tree, result, count
+          };
+      };
+
+      return buildVTree();
   });
 
   log(data);
 
+  // log(chalk.yellow(`The max DOM depth of page "${FETCH_URL}" is ${data.depth}.`));
   await browser.close();
 })();
