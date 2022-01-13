9a89d025595ea0574bf2f8c6673f9430a62ece0b
Andy Chen <chenyangpoppin@gmail.com>
2022-01-13 09:23:12 +0800

    chore: remove unused code

--- index3.js
+++ index3.js
@@ -7,7 +7,6 @@ function find_max(nums) {
   let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
   for (let num of nums) {
     if (num > max_num) {
-      // (Fill in the missing line here)
       max_num = num;
     }
   }
